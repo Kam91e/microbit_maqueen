9e36b439cdd3c369df5ef79bc3368597882139e0
Kam91e <43109694+Kam91e@users.noreply.github.com>
2019-12-26 13:19:06 +0100

    Update Police.js

--- Police.js
+++ Police.js
@@ -2,8 +2,15 @@ input.onButtonPressed(Button.A, function () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
     basic.pause(100)
-    maqueen.motorStop(maqueen.Motors.M1)
-    maqueen.motorStop(maqueen.Motors.M1)
+    maqueen.motorStop(maqueen.Motors.All)
+})
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
 })
 basic.forever(function () {
     basic.showLeds(`
@@ -13,14 +20,6 @@ basic.forever(function () {
         # # # # #
         . . . . .
         `)
-})
-basic.forever(function () {
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    basic.pause(100)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    basic.pause(100)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
 })
 basic.forever(function () {
     music.playTone(220, music.beat(BeatFraction.Whole))
